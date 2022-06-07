f77a9333cc65b64c99981f16a1708387deacb47d
Andriy Oleksievets <aandriy335@gmail.com>
2022-06-07 10:11:46 +0300

    Added TodoList to LocalStorage & rendering it when the page is refreshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { TODO_ITEMS } from './constants';
 
 import Header from './components/Header/Header';
 import CreateTodo from './components/CreateTodo/CreateTodo';
@@ -9,12 +11,31 @@ import TodoList from './components/TodoList/TodoList';
 
 /**
  * TODO:
- * - Add todoList to LocalStorage
  * - Add trash list for removed todos
  */
 
 function App() {
 	const [todos, setTodos] = useState([]);
+
+	const saveTodosToLocalStorage = () => {
+		if (todos.length) localStorage.setItem(TODO_ITEMS, JSON.stringify(todos));
+	};
+
+	const getTodosFromLocalStorage = () => {
+		const savedTodos = localStorage.getItem(TODO_ITEMS);
+
+		if (savedTodos) {
+			setTodos(JSON.parse(savedTodos));
+		}
+	};
+
+	useEffect(() => {
+		getTodosFromLocalStorage();
+	}, []);
+
+	useEffect(() => {
+		saveTodosToLocalStorage();
+	}, [todos]);
 
 	return (
 		<div className='container'>
